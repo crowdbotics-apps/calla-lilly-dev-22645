d72efd05a3d850c5b9ccf1876b4bcfcccb961ee5
Crowdbotics <team@crowdbotics.com>
2021-06-21 19:28:43 +0000

    Update models

--- backend/calla_lilly_dev_22645/wsgi.py
+++ backend/calla_lilly_dev_22645/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calla_lilly_dev_22645.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calla_lilly_dev_22645.settings")
 
 application = get_wsgi_application()
